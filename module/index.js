a304c1107425a9987c592089e4de73a430d96ebc
Ely De La Cruz <elycruz@elycruz.com>
2013-10-07 12:48:32 -0400

    Added module sub generator and it's template.

---
+++ module/index.js
@@ -0,0 +1,36 @@
+/*jshint latedef:false */
+var generator  = require('yeoman-generator');
+var util       = require('util');
+var path       = require('path');
+var validDir = require('../helpers/validateDirectory');
+var fs = require('fs');
+
+module.exports = Generator;
+
+function Generator() {
+  generator.NamedBase.apply(this, arguments);
+  var dirPath = '../templates/javascript';
+  this.sourceRoot(path.join(__dirname, dirPath));
+
+  // invoke  mocha
+  this.hookFor('mocha-amd', {
+    as: 'unitTest',
+    args: [this.name, 'module', 'modules']
+  });
+}
+
+util.inherits(Generator, generator.NamedBase);
+
+Generator.prototype.createModuleFiles = function createModuleFiles() {
+  var ext = 'js',
+	  baseDir = validDir.getValidatedFolder( 'app/'),
+	  modulesDir;
+
+	// If no module folder create it
+	if (validDir.getValidatedFolder(baseDir + 'scripts/modules').length <= 0) {
+		fs.mkdir(baseDir + 'scripts/modules');
+	}
+
+  this.template('module.' + ext, path.join(baseDir
+      + 'scripts/modules', this.name + '.' + ext));
+};
---
+++ templates/javascript/module.js
@@ -0,0 +1,15 @@
+define([
+    'application'
+], function(app) {
+
+	'use strict';
+
+    app.module('<%= name %>', function(mod, app) {
+        mod.addInitializer(function() {
+	        console.log('<%= _.capitalize(name) %> has been initialized.');
+        });
+    });
+
+    return app.module('<%= name %>');
+
+});
